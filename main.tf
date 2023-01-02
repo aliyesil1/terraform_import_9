4ce01050173058306e197f35e0a656a5582d55f0
Ali Yesil <aliyesiluk@yahoo.com>
2023-01-02 23:55:38 +0000

    changed

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "api_rg" {
     name = var.rg_name
     location = var.rg_location  
 }
+
+resource "azurerm_resource_group" "ali_import9_test" {
+  # (resource arguments)
+}
